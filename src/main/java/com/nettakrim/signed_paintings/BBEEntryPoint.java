c95836cd36ffc2905593b16c06068565258ab06a
ExtraJuiceMan <onomatopoeicboom@gmail.com>
2026-07-10 13:58:59 -0700

    Fix BBE support for BBE 1.3.7
    The BBE mixins were a workaround for BBE's alternate renderers not working as they did before in 1.3.6. BBE 1.3.7 fixes this so the alternate renderer has been re-added and mixins are no longer necessary.

--- src/main/java/com/nettakrim/signed_paintings/BBEEntryPoint.java
+++ src/main/java/com/nettakrim/signed_paintings/BBEEntryPoint.java
@@ -8,15 +8,18 @@ import betterblockentities.render.AltRendererProvider;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.nettakrim.signed_paintings.access.BannerBlockEntityRenderStateAccessor;
 import com.nettakrim.signed_paintings.access.OverlayInfoAccessor;
+import com.nettakrim.signed_paintings.access.SignBlockEntityAccessor;
 import com.nettakrim.signed_paintings.rendering.OverlayInfo;
 import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.blockentity.BannerRenderer;
 import net.minecraft.client.renderer.blockentity.state.BannerRenderState;
+import net.minecraft.client.renderer.blockentity.state.SignRenderState;
 import net.minecraft.client.renderer.feature.ModelFeatureRenderer;
 import net.minecraft.client.renderer.state.level.CameraRenderState;
 import net.minecraft.world.level.block.BannerBlock;
 import net.minecraft.world.level.block.WallBannerBlock;
 import net.minecraft.world.level.block.entity.BannerBlockEntity;
+import net.minecraft.world.level.block.entity.SignBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
 
@@ -24,6 +27,30 @@ public class BBEEntryPoint implements BBEApiEntryPoint {
     @Override
     public void registerRenderers(AltRendererRegistration context) {
         context.registerRenderer(SupportedBlockEntityTypes.BANNER, BBEBannerRenderer::new);
+        context.registerRenderer(SupportedBlockEntityTypes.SIGN, BBESignRenderer::new);
+    }
+
+    private static class BBESignRenderer implements AltRenderer<SignBlockEntity, SignRenderState> {
+        public BBESignRenderer(final AltRendererProvider.Context context) {
+        }
+
+        public SignRenderState createRenderState() {
+            return new SignRenderState();
+        }
+
+        public void extractRenderState(final SignBlockEntity blockEntity, final SignRenderState renderState, final float partialTicks, final Vec3 cameraPosition, final ModelFeatureRenderer.CrumblingOverlay breakProgress) {
+            AltRenderer.super.extractRenderState(blockEntity, renderState, partialTicks, cameraPosition, breakProgress);
+            SignedPaintingsClient.paintingRenderer.modifySignRenderState(blockEntity, renderState);
+        }
+
+        public void submit(final SignRenderState state, final PoseStack poseStack, final SubmitNodeCollector submitNodeCollector, final CameraRenderState camera) {
+            SignedPaintingsClient.paintingRenderer.renderSignPaintings(state, poseStack, submitNodeCollector);
+        }
+
+        @Override
+        public boolean shouldRender(SignBlockEntity blockEntity, Vec3 cameraPosition) {
+            return SignedPaintingsClient.paintingRenderer.renderWithReducedCulling((SignBlockEntityAccessor)blockEntity) || AltRenderer.super.shouldRender(blockEntity, cameraPosition);
+        }
     }
 
     private static class BBEBannerRenderer implements AltRenderer<BannerBlockEntity, BannerRenderState> {
